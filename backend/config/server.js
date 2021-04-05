5db4ba489d63060173e33ec599d55ee30fbd4e7b
rgrace9 <rgracewilliams@gmail.com>
2021-04-04 22:37:51 -0400

    Set up environment variables

--- backend/config/server.js
+++ backend/config/server.js
@@ -1,9 +1,9 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  host: `${process.env.HOST}`,
+  port: process.env.PORT,
   admin: {
     auth: {
-      secret: env('ADMIN_JWT_SECRET', '825383f33d74367787afbf8594530a40'),
+      secret: `${process.env.ADMIN_JWT_SECRET}`,
     },
   },
 });
---
+++ backend/server.js
@@ -0,0 +1,5 @@
+const dotenv = require('dotenv');
+const strapi = require('strapi');
+
+dotenv.config();
+strapi().start();
