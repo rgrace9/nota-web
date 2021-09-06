b9791d689e790cd5f7f6dc6caca9074fbbee2e43
rgrace9 <rgracewilliams@gmail.com>
2021-09-05 22:20:33 -0400

    Begin to create the lesson plans search result container.

---
+++ pages/lesson-plans/LessonPlansSearchResults.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const LessonPlansSearchResults = props => {
+  return (
+    <div>
+      
+    </div>
+  );
+};
+
+LessonPlansSearchResults.propTypes = {
+  
+};
+
+export default LessonPlansSearchResults;
