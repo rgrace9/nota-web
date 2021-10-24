3b8302ff00fed76ed11f2d564e7ac392c16c7924
rgrace9 <rgracewilliams@gmail.com>
2021-10-23 23:58:48 -0400

    Scaffold the translations page.

--- pages/transcriptions/index.js
+++ pages/transcriptions/index.js
@@ -1,9 +1,38 @@
-import React from 'react'
-import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
-import Layout from '@/components/Layout';
-import ContentLayout from '@/components/Layout/ContentLayout';
+import React, {useEffect, useMemo, useState} from "react";
+import PropTypes from "prop-types";
+import { serverSideTranslations } from "next-i18next/serverSideTranslations";
+import { SearchFiltersContainer } from "@/components/shared/SearchFilters";
+import styled from "@emotion/styled";
+import StrapiClient from "@/lib/StrapiClient";
+import Layout from "@/components/Layout";
+import ContentLayout from "@/components/Layout/ContentLayout";
+import { ListBox } from "@/components/shared/dataEntry";
+import { AUTHOR_OPTIONS, PRICES } from "@/constants/index";
+import { PrimaryButton } from "@/components/shared/Button";
+import { formatQuery } from 'utils/queryString';
+import { withRouter } from 'next/router'
+import qs from 'qs'
+import { device } from "@/styles/screenSizes";
+import { useListBox } from "@/utils/hooks";
+import LessonPlansSearchResults from "@/features/LessonPlansSearchResults";
 
-const Transcriptions = () => {
+
+const STRAPI_CLIENT = new StrapiClient();
+
+const Transcriptions = (props) => {
+  const {
+    router,
+    authorOptions,
+    lessonPlans
+  } = props;
+
+  const { asPath, query } = router;
+  const queryString = JSON.stringify(query);
+  const queryParams = useMemo(() => qs.parse(query), [queryString]);
+
+  const handleLessonPlansSearch = () => {
+    // e.preventDefault();
+  };  
   return (
     <Layout
     pageTitle='Transcriptions'
--- pages/translations/index.js
+++ pages/translations/index.js
@@ -1,25 +1,92 @@
-import React from 'react'
-import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
-import Layout from '@/components/Layout';
-import ContentLayout from '@/components/Layout/ContentLayout';
+import React, {useEffect, useMemo, useState} from "react";
+import PropTypes from "prop-types";
+import { serverSideTranslations } from "next-i18next/serverSideTranslations";
+import { SearchFiltersContainer } from "@/components/shared/SearchFilters";
+import styled from "@emotion/styled";
+import StrapiClient from "@/lib/StrapiClient";
+import Layout from "@/components/Layout";
+import ContentLayout from "@/components/Layout/ContentLayout";
+import { ListBox } from "@/components/shared/dataEntry";
+import { AUTHOR_OPTIONS, PRICES } from "@/constants/index";
+import { PrimaryButton } from "@/components/shared/Button";
+import { formatQuery } from 'utils/queryString';
+import { withRouter } from 'next/router'
+import qs from 'qs'
+import { device } from "@/styles/screenSizes";
+import { useListBox } from "@/utils/hooks";
+import LessonPlansSearchResults from "@/features/LessonPlansSearchResults";
 
-const Translations = () => {
+
+const STRAPI_CLIENT = new StrapiClient();
+
+const Translations = (props) => {
+  const {
+    router,
+    authorOptions,
+    lessonPlans
+  } = props;
+  
+  const { asPath, query } = router;
+  const queryString = JSON.stringify(query);
+  const queryParams = useMemo(() => qs.parse(query), [queryString]);
+
+  const handleLessonPlansSearch = () => {
+    // e.preventDefault();
+  };
   return (
-    <Layout
-    pageTitle='Translations'
-  >
-    <ContentLayout
-      title='Translations'
-    >
+    <Layout pageTitle="Translations">
+      <ContentLayout maxWidth='1000px' title="Translations">
+        <SearchFiltersContainer>
+          <form onSubmit={handleLessonPlansSearch}>
+          <StyledFormRow>
+            <StyledOptionContainer>
+              <ListBox
+                allObject={{ name: "All Authors", id: "all" }}
+                labelText="Author"
+                labelValue="author"
+                options={AUTHOR_OPTIONS}
+              />
+            </StyledOptionContainer>
+          </StyledFormRow>
+
+            <div>
+              <PrimaryButton type="submit" text="Search" />
+            </div>
+          </form>
+        </SearchFiltersContainer>
+          
+           
+            <LessonPlansSearchResults results={lessonPlans} />
+         
       </ContentLayout>
-      </Layout>
+    </Layout>
   );
 };
 
-export default Translations;
+Translations.propTypes = {};
 
-export const getStaticProps = async ({ locale }) => ({
-  props: {
-    ...await serverSideTranslations(locale, ['common', 'nav', 'home']),
-  },
-})
+export default withRouter(Translations);
+
+export const getStaticProps = async ({ locale }) => {
+  const lessonPlans = await STRAPI_CLIENT.fetchAPI("lesson-plans");
+  const authorOptions = await STRAPI_CLIENT.fetchAPI("authors");
+
+  return {
+    props: {
+      ...(await serverSideTranslations(locale, ["common", "nav", "home"])),
+      lessonPlans,
+      authorOptions,
+    },
+  }
+};
+
+const StyledFormRow = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  width: 100%;
+`
+
+const StyledOptionContainer = styled.div`
+  width: 45%;
+`
